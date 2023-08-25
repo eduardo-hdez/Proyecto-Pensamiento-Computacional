b338971d3530da284712855816bc0e91a978c707
Eduardo Hernández <98539663+eduardo-hdez@users.noreply.github.com>
2023-08-24 23:38:53 -0600

    Subir archivo avance 1

---
+++ Proyecto_Traductor.py
@@ -0,0 +1,39 @@
+"""
+
+Las variables mostradas guardan el código morse de cada una de letras del alfabeto sin
+contar la letra "ñ".
+
+"""
+
+a = ".-"
+b = "-..."
+c = "-.-."
+d = "-.."
+e = "."
+f = "..-."
+g = "--."
+h = "...."
+i = ".."
+j = ".---"
+k = "-.-"
+L = ".-.."
+m = "--"
+n = "-."
+o = "---"
+p = ".--."
+q = "--.-"
+r = ".-."
+s = "..."
+t = "-"
+u = "..-"
+v = "...-"
+w = ".--"
+x = "-..-"
+y = "-.--"
+z = "--.."
+
+
+print("Texto a Código Morse = 1")
+print("Código Morse a Texto = 2")
+
+opcion = int(input("Seleccione un número: "))
