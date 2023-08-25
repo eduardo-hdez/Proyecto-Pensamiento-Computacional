de3ca0a21ada67091c993902e913cbb0750e69b0
Eduardo Hernández <98539663+eduardo-hdez@users.noreply.github.com>
2023-08-24 23:44:47 -0600

    Update Proyecto_Traductor.py

--- Proyecto_Traductor.py
+++ Proyecto_Traductor.py
@@ -1,7 +1,11 @@
 """
 
 Las variables mostradas guardan el código morse de cada una de letras del alfabeto sin
-contar la letra "ñ".
+contar la letra "ñ". (En mi caso, no aplicaría operadores aritmeticos debido a que el
+programa no necesita de ellos para hacer un cálculo, aún así, decidí hacer este pequeño
+avance de denominar las variables con su respectivo código morse para que en futuras
+clases, ya tengo un conocimiento mpás vasto sobre Python y así agregar los operadores
+que mi código necesita)
 
 """
 
