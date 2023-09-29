3401536ab9be098363fff59604ecc0a91453c626
Eduardo Hernández <98539663+eduardo-hdez@users.noreply.github.com>
2023-09-28 23:56:48 -0600

    Add files via upload

--- Proyecto_Traductor.py
+++ Proyecto_Traductor.py
@@ -1,170 +1,73 @@
-#Funciones
-
-def txt_a_morse():
-    txt = str(input("Introduce una letra para ser traducido a"
-                    "\ncódigo morse: ")).lower()
-    
-    if txt == "a":
-        return ".-"
-    elif txt == "b":
-        return "-..."
-    elif txt == "c":
-        return "-.-."
-    elif txt == "d":
-        return "-.."
-    elif txt == "e":
-        return "."
-    elif txt == "f":
-        return "..-."
-    elif txt == "g":
-        return "--."
-    elif txt == "h":
-        return "...."
-    elif txt == "i":
-        return ".."
-    elif txt == "j":
-        return ".---"
-    elif txt == "k":
-        return "-.-"
-    elif txt == "l":
-        return ".-.."
-    elif txt == "m":
-        return "--"
-    elif txt == "n":
-        return "-."
-    elif txt == "ñ":
-        return "--.--"
-    elif txt == "o":
-        return "---"
-    elif txt == "p":
-        return ".--."
-    elif txt == "q":
-        return "--.-"
-    elif txt == "r":
-        return "-.-"
-    elif txt == "s":
-        return "..."
-    elif txt == "t":
-        return "-"
-    elif txt == "u":
-        return "..-"
-    elif txt == "v":
-        return "...-"
-    elif txt == "w":
-        return ".--"
-    elif txt == "x":
-        return "-..-"
-    elif txt == "y":
-        return "-.--"
-    elif txt == "z":
-        return "--.."
-    else:
-        return "El texto no es válido. INTENTE DE NUEVO."
-
-    
-def morse_a_txt():
-    txt2 = str(input("Introduce un código morse para ser"
-                     "\ntraducido a texto: ")).lower()
-    
-    if txt2 == ".-":
-        return "a"
-    elif txt2 == "-...":
-        return "b"
-    elif txt2 == "-.-.":
-        return "c"
-    elif txt2 == "-..":
-        return "d"
-    elif txt2 == ".":
-        return "e"
-    elif txt2 == "..-.":
-        return "f"
-    elif txt2 == "--.":
-        return "g"
-    elif txt2 == "....":
-        return "h"
-    elif txt2 == "..":
-        return "i"
-    elif txt2 == ".---":
-        return "j"
-    elif txt2 == "-.-":
-        return "k"
-    elif txt2 == ".-..":
-        return "l"
-    elif txt2 == "--":
-        return "m"
-    elif txt2 == "-.":
-        return "n"
-    elif txt2 == "--.--":
-        return "ñ"
-    elif txt2 == "---":
-        return "o"
-    elif txt2 == ".--.":
-        return "p"
-    elif txt2 == "--.-":
-        return "q"
-    elif txt2 == "-.-":
-        return "r"
-    elif txt2 == "...":
-        return "s"
-    elif txt2 == "-":
-        return "t"
-    elif txt2 == "..-":
-        return "u"
-    elif txt2 == "...-":
-        return "v"
-    elif txt2 == ".--":
-        return "w"
-    elif txt2 == "-..-":
-        return "x"
-    elif txt2 == "-.--":
-        return "y"
-    elif txt2 == "--..":
-        return "z"
-    else:
-        return "El código morse no es válido. INTENTE DE NUEVO."
-
 """
--------------------------------------------------------------------------------
+En esta función, lo que hago es almacenar los datos del código morse y las
+letras en el alfabeto en español en listas para luego ser usadas más adelante
 """
 
-
-#Loops
-
-while True:
-    opcion = input("Introduzca el número de opción que desee usar:"
-    "\n1. Texto a código Morse"
-    "\n2. Código Morse a texto"
-    "\n3. Salir del programa"
-    "\n")
+def datos():
+    codigo_morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+                    "..", ".---", "-.-", ".-..", "--", "-.", "--.--" "---",
+                    ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+                    "-..-", "-.--", "--.."]
     
-    if opcion == "1":
-        print("El código morse de este texto es:", txt_a_morse())
-
-    elif opcion == "2":
-        print("El texto de este código morse es:", morse_a_txt())
-
-    elif opcion == "3":
-        print("¡Adiós!")
-        break
+    letras_espanol = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                      "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U",
+                      "V", "W","X", "Y", "Z"]
     
-    else:
-        print("Esta opción no es válida. INTENTE DE NUEVO.")
-        opcion = (input("Introduzca el número de opción que desee usar:"
-        "\n1. Texto a código Morse"
-        "\n2. Código Morse a texto"
-        "\n3. Salir del programa"
-        "\n"))
-
+    return codigo_morse, letras_espanol
 
 """
-morse_txt = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
-              '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
-              '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
-              '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
-              '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
-              '--..': 'Z'}
+En esta función, se hace la traducción de texto a código morse usando un ciclo
+"for" y con la ayuda de las funciones upper() y strip() para que el texto
+ingresado por el usuario no limite a este a escribir su texto de una manera
+en la yo le indique. Esto hace que el usuario pueda tener más libertad a la
+hora de escribir su texto y traducirlo a código morse.
 """
 
+def traduccion_texto(palabra):
+    codigo_morse, letras_espanol = datos()
 
-
+    texto = ""
     
+    for x in palabra.upper():
+        if x == " ":
+            texto = texto + "/"
+        elif x in letras_espanol:
+            i = letras_espanol.index(x)
+            letras_morse = codigo_morse[i]
+            texto = texto + letras_morse + " "
+
+    return texto.strip()
+
+"""
+En esta función, lo que hago es la parte del menú del programa donde hago que
+la consola imprima las opciones que el usuario pueda ingresar. Luego, si la
+opcion es "1", el programa manda al usuario a la funcion traduccion_texto para
+que el texto ingresado por el usuario sea traducido a código morse. Si la
+opcion es "2", el programa manda al usuario a la funcion traduccion_morse para
+que el código morse ingresado por el usuario sea traducido a texto. Si la
+opción es "3" el programa saldrá del while loop y se cerrará.
+"""
+
+def menu():
+    while True:
+        print(" \nMenú:")
+        print("1) Traductor de Texto a Código Morse")
+        print("2) Traductor de Código Morse a Texto (Aún no está listo)")
+        print("3) Salir \n")
+
+        opcion = input("Elija una opción del 1 al 3: ")
+
+        if opcion == "1":
+            frase = input("Ingrese un texto en español: ")
+            morse = traduccion_texto(frase)
+            print("La traducción del texto a código morse es:\n", morse)
+        elif opcion == "2":
+            print("¡Error! Esta opción aún no está disponible.")
+        elif opcion == "3":
+            print("¡Adiós! Gracias por usar este programa.")
+            break
+        else:
+            print("Opción incorrecta. INTENTE DE NUEVO.")
+
+
+menu()
