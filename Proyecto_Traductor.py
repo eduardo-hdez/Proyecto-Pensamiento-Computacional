f62c37c2696724652c6b96a670c8e770865e8451
Eduardo Hernández <98539663+eduardo-hdez@users.noreply.github.com>
2023-10-11 12:47:34 -0600

    Add files via upload

--- Proyecto_Traductor.py
+++ Proyecto_Traductor.py
@@ -1,16 +1,32 @@
 """
-En esta función, lo que hago es almacenar los datos del código morse y las
-letras en el alfabeto en español en listas para luego ser usadas más adelante
+Proyecto Libre Python
+Traductor de Código Morse.
+El programa recibe el texto del usuario y lo traduce a código morse, también
+tiene como segunda opción, recibir múltiples código morse y traducirlos
+a texto.
+"""
+
+"""
+======================== Función para almacenar varibales =====================
 """
 
 def datos():
+    
+    """
+    (Uso de funciones y listas anidadas)
+    La función se encarga de almancear en 2 listas anidades, los valores de
+    cada código morse con su respectiva letra en el alfabeto y también algunos
+    carácteres especiales.
+    """
+    
     codigo_morse_letras = [
         [".-", "A"], ["-...", "B"], ["-.-.", "C"], ["-..", "D"], [".", "E"],
         ["..-.", "F"], ["--.", "G"], ["....", "H"], ["..", "I"], [".---", "J"],
         ["-.-", "K"], [".-..", "L"], ["--", "M"], ["-.", "N"], ["---", "O"],
         [".--.", "P"], ["--.-", "Q"], [".-.", "R"], ["...", "S"], ["-", "T"],
-        ["..-", "U"], ["...-", "V"], [".--", "W"], ["-..-", "X"], ["-.--", "Y"],
-        ["--..", "Z"]]
+        ["..-", "U"], ["...-", "V"], [".--", "W"], ["-..-", "X"],
+        ["-.--", "Y"], ["--..", "Z"]
+        ]
     
     codigo_morse_especiales = [
         [".----", "1"], ["..---", "2"], ["...--", "3"], ["....-", "4"],
@@ -19,20 +35,28 @@ def datos():
         ["..--..", "?"], [".----.", "'"], ["-.-.--", "!"], ["-..-.", "/"],
         ["-.--.", "("], ["-.--.-", ")"], [".-...", "&"], ["---...", ":"],
         ["-.-.-.", ";"], ["-...-", "="], [".-.-.", "+"], ["-....-", "-"],
-        ["..--.-", "_"], [".-..-.", '"'], ["...-..-", "$"], [".--.-.", "@"]]
+        ["..--.-", "_"], [".-..-.", '"'], ["...-..-", "$"], [".--.-.", "@"]
+        ]
 
     return codigo_morse_letras, codigo_morse_especiales
 
 """
-En estas dos funciones, se hace la traducción de texto a código morse y de 
-texto a código morseusando un ciclo "for" y con la ayuda de las funciones 
-upper() y strip() para que el texto ingresado por el usuario no limite a 
-este a escribir su texto de una manera en la yo le indique. Esto hace que 
-el usuario pueda tener más libertad a la hora de escribir su texto y 
-traducirlo a código morse y viceversa.
+=================== Funciones para traducir texto o código morse ==============
 """
 
 def traduccion_texto(palabra):
+
+    """
+    (Uso de funciones, uso de operadores, ciclos, condicionales
+    y condicionlaes anidadas)
+    Recibe: palabra
+    Hace la traducción de texto a código morse mediante usando ciclos que
+    revisen la función datos() donde los elementos son guardados y da como
+    resultado un texto totalemente traducido sin importar las mayúsculas
+    o minúsculas y separando cada código morse con una diagonal "/".
+    Devuelve: Código morse traducido del texto
+    """
+    
     codigo_morse_letras, codigo_morse_especiales = datos()
 
     texto = ""
@@ -54,6 +78,19 @@ def traduccion_texto(palabra):
     return texto.strip()
 
 def traduccion_morse(texto_morse):
+
+    """
+    (Uso de funciones, uso de operadores, ciclos, ciclos anidados,
+    condicionales y condicionlaes anidadas)
+    Recibe: texto_morse
+    Hace la traducción de código morse a texto mediante usando ciclos para
+    revisar la función datos() donde se encuentran almacenados los elementos de
+    cada letra con su respectivo código morse, al terminar, da como resultado
+    el texto del código morse todo junto sin espacios y no cuenta los
+    carácteres espceciales.
+    Devuelve: Texto traducido del código morse
+    """
+     
     codigo_morse_letras, codigo_morse_especiales = datos()
     
     palabras = texto_morse.split("/")
@@ -76,16 +113,19 @@ def traduccion_morse(texto_morse):
     return texto.strip()
 
 """
-En esta función, lo que hago es la parte del menú del programa donde hago que
-la consola imprima las opciones que el usuario pueda ingresar. Luego, si la
-opcion es "1", el programa manda al usuario a la funcion traduccion_texto para
-que el texto ingresado por el usuario sea traducido a código morse. Si la
-opcion es "2", el programa manda al usuario a la funcion traduccion_morse para
-que el código morse ingresado por el usuario sea traducido a texto. Si la
-opción es "3" el programa saldrá del while loop y se cerrará.
+=============================== Menú del programa =============================
 """
 
 def menu():
+
+    """
+    (Uso de funciones, ciclos y condicionales)
+    Inicializa el menú dando al usuario libertad de ingresar un número
+    del 1 al 3, donde cada uno tiene un diferente resultado, el usuario puede
+    seguir usando el programa hasta que este ingrese la opcion número 3 donde
+    el programa se cierra.
+    """
+    
     while True:
         print(" \nMenú:")
         print("1) Traductor de Texto a Código Morse")
@@ -95,11 +135,13 @@ def menu():
         opcion = input("Elija una opción del 1 al 3: ")
 
         if opcion == "1":
-            frase = input("Ingrese un texto en español: ")
+            frase = input("Ingrese un texto: ")
             morse = traduccion_texto(frase)
             print("La traducción del texto a código Morse es:\n", morse)
         elif opcion == "2":
-            morse = input("Ingrese un código Morse: ")
+            morse = input("(ÚNICAMENTE INGRESE LETRAS)"
+                          "\n(Separe cada código morse con un espacio)"
+                          "\nIngrese un código Morse: ")
             texto = traduccion_morse(morse)
             print("La traducción de código Morse a texto es:\n", texto)
         elif opcion == "3":
@@ -109,4 +151,3 @@ def menu():
             print("Opción incorrecta. INTENTE DE NUEVO.")
 
 menu()
-
